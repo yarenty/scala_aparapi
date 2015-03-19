037da8db4be82c683529555535648e6b09d00f81
yarenty <yarenty@jaros>
2015-03-19 17:23:03 +0000

    and some additional tests.... need to go with simple structures before execution on kernel!!

--- src/main/java/com/yarenty/java/aparapi/MultiplicityStructureKernel.java
+++ src/main/java/com/yarenty/java/aparapi/MultiplicityStructureKernel.java
@@ -16,6 +16,30 @@ import static  scala.collection.JavaConversions.*;
 public class MultiplicityStructureKernel {
 
 
+	/***
+	 * NOT WORKING!!!
+	 * 
+	 * OUTPUT:
+	 * <code>
+com.amd.aparapi.internal.exception.ClassParseException: @0 NEW We don't support new instructions
+	at com.amd.aparapi.internal.model.MethodModel.createListOfInstructions(MethodModel.java:280)
+	at com.amd.aparapi.internal.model.MethodModel.init(MethodModel.java:1656)
+	at com.amd.aparapi.internal.model.MethodModel.<init>(MethodModel.java:1472)
+	at com.amd.aparapi.internal.model.ClassModel.getMethodModel(ClassModel.java:2589)
+	at com.amd.aparapi.internal.model.ClassModel.getEntrypoint(ClassModel.java:2622)
+	at com.amd.aparapi.internal.model.ClassModel.getEntrypoint(ClassModel.java:2631)
+	at com.amd.aparapi.internal.kernel.KernelRunner.execute(KernelRunner.java:935)
+	at com.amd.aparapi.Kernel.execute(Kernel.java:1985)
+	at com.amd.aparapi.Kernel.execute(Kernel.java:1916)
+	at com.amd.aparapi.Kernel.execute(Kernel.java:1886)
+	at com.yarenty.java.aparapi.MultiplicityStructureKernel.go(MultiplicityStructureKernel.java:36)
+	at com.yarenty.scala.aparapi.main$.main(main.scala:64)
+	at com.yarenty.scala.aparapi.main.main(main.scala)
+	 * </code>
+	 * 
+	 * @param in
+	 * @return
+	 */
 	
 	public float[] go(final List<Tuple2<Long, ComplicatedStructure>> in) {
 		
--- src/main/java/com/yarenty/java/aparapi/MultiplicityToupleKernel.java
+++ src/main/java/com/yarenty/java/aparapi/MultiplicityToupleKernel.java
@@ -16,19 +16,28 @@ import static  scala.collection.JavaConversions.*;
 public class MultiplicityToupleKernel {
 
 
-	
-	
 	public float[] go(final List<Tuple2<Long, Float>> in) {
 		
-//		final int size = 100;
-//		final float in[] = new float[size]; // initialization of in[0..8191] omitted
 		final float out[] = new float[in.size()]; //out[in.length];
-
 		
-		Kernel kernel = new Kernel(){
+
+		final float[] x = new float[in.size()];
+		  
+		 for(int i = 0; i<x.length; i++) {
+			  x[i] = in.get(i)._2;
+		  }
+		
+
+		  Kernel kernel = new Kernel(){
+			
+			
 		   @Override public void run(){
 		      int i = getGlobalId();
-		      out[i]=in.get(i)._2 *in.get(i)._2;
+		      // yes working only on simple structures!!
+		      // this one:
+		      // out[i]= in.get(i)._2 * in.get(i)._2;
+		      // doesnt work!! - @see MultiplicityStructureKernel 
+		      out[i]= x[i] * x[i];
 		   }
 		};
 		
